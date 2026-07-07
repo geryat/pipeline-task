0b7611cfc5c8c7310e83d1d615f3c30521eeabdd
geryat <gery@travelio.com>
2026-07-07 11:51:30 +0700

    feat(app): redesign API response format #major

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,22 @@
 # Changelog
 ## unreleased
 
+### Bug Fixes
+
+- skip Slack notification if webhook not configured (30790c2)
+
+- minor update #patch (5997496)
+
+- flexible paths filter and lint for any folder structure (f324793)
+
+
 ### Chores
 
 - remove actions-runner from tracking and add to gitignore (f10838a)
 
 - clean up before history rewrite (58d1e0e)
+
+- move flake8 config inline, remove .flake8 file (19f0856)
 
 
 ### Features
--- testapp.py
+++ testapp.py
@@ -12,10 +12,17 @@ def client():
 def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json["message"] == "Hello from Flask in Docker!"
+    assert response.json["status"] == "success"
+    assert response.json["data"]["message"] == "Hello from Flask in Docker!"
 
 
 def test_health(client):
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json["status"] == "ok"
+
+
+def test_version(client):
+    response = client.get("/version")
+    assert response.status_code == 200
+    assert response.json["version"] == "1.0.0"
